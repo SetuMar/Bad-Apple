7c7c06b55da15ec3c35eb54f3b67b01f2a6343d1
Setu Marathe <82828034+SetuMar@users.noreply.github.com>
2022-11-05 12:57:34 -0400

    All files used for Bad Apple! in python
    Note: A folder for the frames will need to be created. It may take some time to get all the frames. You have been warned.

--- console-writer.py
+++ console-writer.py
@@ -1,6 +1,11 @@
 import os
 import time
 import custom_ascii_converter
+import pygame
+
+pygame.init()
+
+pygame.mixer.music.load('bad_apple.mp3')
 
 image_locations = None
 
@@ -15,13 +20,16 @@ for loc in image_locations:
 index_image = dict(sorted(index_image.items()))
 sorted_image_location = index_image.values()
 
+print("loading...")
+
 time.sleep(3)
+pygame.mixer.music.play()
 
 for image_loc in sorted_image_location:
     full_path = 'video-frames/' + image_loc
     
     custom_ascii_converter.generate_ascii_image(full_path, 15)
     
-    time.sleep(0.02)
+    time.sleep(0.024)
 
 print("VIDEO OVER. CLOSE THE CONSOLE, WEABOO")
